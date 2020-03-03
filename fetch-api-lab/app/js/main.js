eb4c4ba7064f2a658afc509fa6286f34beff7d90
Damian de Vega <damiandevega@gmail.com>
2020-03-03 14:51:32 -0500

    adding custom headers to post request

--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -115,10 +115,13 @@ headButton.addEventListener('click', headRequest);
 
 /* NOTE: Never send unencrypted user credentials in production! */
 function postRequest() {
-  const formData = new FormData(document.getElementById('msg-form'));
+  // const formData = new FormData(document.getElementById('msg-form'));
+  const messageHeaders = new Headers({ 'Content-Type': 'application/json', 'X-CUSTOM': 'hello world', });
+
   fetch('http://localhost:5000/', {
     method: 'POST',
-    body: formData
+    headers: messageHeaders,
+    body: JSON.stringify({ lab: 'fetch', status: 'fun' })
   })
     .then(validateResponse)
     .then(readResponseAsText)
