af1b456166ba855a3edc3d0ee77d474cc7456f86
Damian de Vega <damiandevega@gmail.com>
2020-03-21 11:51:49 -0400

    deleting old caches to free up space

--- cache-api-lab/app/sw.js
+++ cache-api-lab/app/sw.js
@@ -19,6 +19,24 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   console.log('Fetch event for ', event.request.url);
   event.respondWith(
