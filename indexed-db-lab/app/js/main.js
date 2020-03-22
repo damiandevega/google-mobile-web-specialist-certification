61b383702a5e41461f91a868e53f89b63300ceeb
Damian de Vega <damiandevega@gmail.com>
2020-03-22 12:26:43 -0400

    adding add product function to indexedDB

--- indexed-db-lab/app/js/main.js
+++ indexed-db-lab/app/js/main.js
@@ -13,17 +13,104 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-var idbApp = (function() {
+var idbApp = (function () {
   'use strict';
 
-  // TODO 2 - check for support
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
 
-  var dbPromise;
+  var dbPromise = idb.open('couches-n-things', 2, function (upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+      // a placeholder case so that the switch block will 
+      // execute when the database is first created
+      // (oldVersion is 0)
+      case 1:
+        console.log('Creating the products object store');
+        upgradeDb.createObjectStore('products', { keyPath: 'id' });
+
+      // TODO 4.1 - create 'name' index
+
+      // TODO 4.2 - create 'price' and 'description' indexes
+
+      // TODO 5.1 - create an 'orders' object store
+
+    }
+  });
 
   function addProducts() {
-
-    // TODO 3.3 - add objects to the products store
-
+    dbPromise.then(function (db) {
+      var tx = db.transaction('products', 'readwrite');
+      var store = tx.objectStore('products');
+      var items = [
+        {
+          name: 'Couch',
+          id: 'cch-blk-ma',
+          price: 499.99,
+          color: 'black',
+          material: 'mahogany',
+          description: 'A very comfy couch',
+          quantity: 3
+        },
+        {
+          name: 'Armchair',
+          id: 'ac-gr-pin',
+          price: 299.99,
+          color: 'grey',
+          material: 'pine',
+          description: 'A plush recliner armchair',
+          quantity: 7
+        },
+        {
+          name: 'Stool',
+          id: 'st-re-pin',
+          price: 59.99,
+          color: 'red',
+          material: 'pine',
+          description: 'A light, high-stool',
+          quantity: 3
+        },
+        {
+          name: 'Chair',
+          id: 'ch-blu-pin',
+          price: 49.99,
+          color: 'blue',
+          material: 'pine',
+          description: 'A plain chair for the kitchen table',
+          quantity: 1
+        },
+        {
+          name: 'Dresser',
+          id: 'dr-wht-ply',
+          price: 399.99,
+          color: 'white',
+          material: 'plywood',
+          description: 'A plain dresser with five drawers',
+          quantity: 4
+        },
+        {
+          name: 'Cabinet',
+          id: 'ca-brn-ma',
+          price: 799.99,
+          color: 'brown',
+          material: 'mahogany',
+          description: 'An intricately-designed, antique cabinet',
+          quantity: 11
+        }
+      ];
+      return Promise.all(items.map(function (item) {
+        console.log('Adding item: ', item);
+        return store.add(item);
+      })
+      ).catch(function (e) {
+        tx.abort();
+        console.log(e);
+      }).then(function () {
+        console.log('All items added successfully!');
+      });
+    });
   }
 
   function getByName(key) {
@@ -34,10 +121,10 @@ var idbApp = (function() {
 
   function displayByName() {
     var key = document.getElementById('name').value;
-    if (key === '') {return;}
+    if (key === '') { return; }
     var s = '';
-    getByName(key).then(function(object) {
-      if (!object) {return;}
+    getByName(key).then(function (object) {
+      if (!object) { return; }
 
       s += '<h2>' + object.name + '</h2><p>';
       for (var field in object) {
@@ -45,8 +132,8 @@ var idbApp = (function() {
       }
       s += '</p>';
 
-    }).then(function() {
-      if (s === '') {s = '<p>No results.</p>';}
+    }).then(function () {
+      if (s === '') { s = '<p>No results.</p>'; }
       document.getElementById('results').innerHTML = s;
     });
   }
@@ -59,15 +146,15 @@ var idbApp = (function() {
 
   function getByDesc() {
     var key = document.getElementById('desc').value;
-    if (key === '') {return;}
+    if (key === '') { return; }
     var range = IDBKeyRange.only(key);
     var s = '';
-    dbPromise.then(function(db) {
+    dbPromise.then(function (db) {
 
       // TODO 4.4b - get items by their description
 
-    }).then(function() {
-      if (s === '') {s = '<p>No results.</p>';}
+    }).then(function () {
+      if (s === '') { s = '<p>No results.</p>'; }
       document.getElementById('results').innerHTML = s;
     });
   }
@@ -80,12 +167,12 @@ var idbApp = (function() {
 
   function showOrders() {
     var s = '';
-    dbPromise.then(function(db) {
+    dbPromise.then(function (db) {
 
       // TODO 5.3 - use a cursor to display the orders on the page
 
-    }).then(function() {
-      if (s === '') {s = '<p>No results.</p>';}
+    }).then(function () {
+      if (s === '') { s = '<p>No results.</p>'; }
       document.getElementById('orders').innerHTML = s;
     });
   }
@@ -97,9 +184,9 @@ var idbApp = (function() {
   }
 
   function fulfillOrders() {
-    getOrders().then(function(orders) {
+    getOrders().then(function (orders) {
       return processOrders(orders);
-    }).then(function(updatedProducts) {
+    }).then(function (updatedProducts) {
       updateProductsStore(updatedProducts);
     });
   }
@@ -117,14 +204,14 @@ var idbApp = (function() {
   }
 
   function updateProductsStore(products) {
-    dbPromise.then(function(db) {
+    dbPromise.then(function (db) {
 
       // TODO 5.7 - update the items in the 'products' object store
 
-    }).then(function() {
+    }).then(function () {
       console.log('Orders processed successfully!');
       document.getElementById('receipt').innerHTML =
-      '<h3>Order processed successfully!</h3>';
+        '<h3>Order processed successfully!</h3>';
     });
   }
 
